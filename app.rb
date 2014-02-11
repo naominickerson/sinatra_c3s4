021bf9bc626ad8bb8eb7a0d6489c4a81def984aa
Tom Close <tom.close@cantab.net>
2014-02-11 13:28:38 +0000

    Initial import

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'pony'
+
+if settings.environment == :production
+  # if we're on heroku, use the sendgrid settings
+  require './production_pony_options'
+else
+  # otherwise, use our normal email account
+  require './development_pony_options'
+end
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  @first_name  = params[:first_name]
+  @second_name = params[:second_name]
+  @email       = params[:email]
+
+  Pony
